33e0b774ad54f95e94546b6d58358b02e080f95b
tck8888 <l>
2020-02-12 23:38:47 +0800

    修改

--- app/src/main/java/com/tck/gradleplugintrain/MainActivity.java
+++ app/src/main/java/com/tck/gradleplugintrain/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.appbar.AppBarLayout;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AppBarLayout.Behavior
     }
 }
